082991044da48a1e31bad7788d6836f1e10cc4f5
Mikhail Pushkin <lukashevichmikhail@gmail.com>
2017-08-29 10:45:15 +0300

    Added header, home and student components

--- myFirstNgApp/src/app/app.module.ts
+++ myFirstNgApp/src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { StudentsComponent } from './students/students.component';
+
+const appRoutes: Routes =[
+         {
+            path: "",
+            redirectTo: "home",
+            pathMatch: "full"
+        },
+    { path: 'home', component: HomeComponent},
+    { path: 'students', component: StudentsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    StudentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
